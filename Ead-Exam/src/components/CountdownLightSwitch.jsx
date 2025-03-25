9e5738792fc753dd8356eb4a20036465c9b65577
ItxUzair1 <muhammaduzair1411@gmail.com>
2025-03-25 10:15:09 +0500

    implement theme functionallity

--- Ead-Exam/src/components/CountdownLightSwitch.jsx
+++ Ead-Exam/src/components/CountdownLightSwitch.jsx
@@ -1,15 +1,23 @@
 import "./CountdownLightSwitch.css"
+import { useState } from "react"
 
 
 export default function CountdownLightSwitch(){
-    return (
-<body>
-  <div className="container">
+    const [isclick,setClick]=useState(false)
+
+    const handleChange=(e)=>{
+        console.log("Hello")
+        setClick(!isclick)
+    }
+return (
+<body className={isclick ? "bg-white" : "bg-black-500"}>
+    {console.log(isclick)}
+  <div className={isclick ? "container bg-white" : "container bg-black"}>
     <div className="header">
       <h1>Countdown & Light Switch</h1>
       <div className="toggle-container">
         <label className="toggle-switch">
-          <input type="checkbox" id="themeToggle" />
+          <input type="checkbox" id="themeToggle" name="togglebutton" onChange={handleChange}/>
           <span className="slider"></span>
         </label>
         <span>Light Mode</span>
@@ -27,6 +35,7 @@ export default function CountdownLightSwitch(){
       </div>
       <div className="message" id="messageArea"></div>
     </div>
+    {console.log(isclick)}
   </div>
 
   
